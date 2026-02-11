b7678cfb85b391813b99086464664bd539632072
Sanath Ramachandran <sanathrs@google.com>
2026-02-11 09:24:11 +0000

    Refined solution for NiceString

--- Kotlin for Java Developers. Week 3/Nice String/Task/src/nicestring/NiceString.kt
+++ Kotlin for Java Developers. Week 3/Nice String/Task/src/nicestring/NiceString.kt
@@ -1,24 +1,15 @@
 package nicestring
 
-fun String.containsAtLeastThreeVowels(): Boolean {
-    val vowels = listOf('a', 'e', 'i', 'o', 'u')
-    return count { c -> c in vowels } >= 3
-}
-
-fun String.containsAtLeastOneDoubleLetter(): Boolean {
-    if(length < 2) return false
-    for(index in 1 until length) {
-        if(this[index] == this[index - 1]) return true
-    }
-    return false
-}
-
-fun String.doesNotContainRestrictedSubstrings(): Boolean {
-    return !(contains("bu") || contains("ba") || contains("be"))
-}
-
-fun Boolean.toInt() = if(this) 1 else 0
-
 fun String.isNice(): Boolean {
-    return (containsAtLeastThreeVowels().toInt() + containsAtLeastOneDoubleLetter().toInt() + doesNotContainRestrictedSubstrings().toInt()) >=2
+    val noBadSubstrings = setOf("bu", "ba", "be").none { this.contains(it) }
+
+    val hasThreeVowels = count { it in "aeiou" } >= 3
+
+    val hasDoubleLetter = zipWithNext().any { it.first == it.second }
+
+    return listOf(noBadSubstrings, hasThreeVowels, hasDoubleLetter).count{it} >= 2
+}
+
+fun main(){
+    println("aza".isNice())
 }
